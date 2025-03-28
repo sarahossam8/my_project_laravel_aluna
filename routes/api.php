148e691c18dfab73caff7416f40e4913168dcd11
sarahossam8 <hossamsara413@gmail.com>
2025-03-28 09:13:18 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/text/{id}', [NotesController::class, 'store']);
+    Route::post('/text', [NotesController::class, 'store']);
     Route::get('/text/{id}', [NotesController::class, 'show']);
     Route::put('/text/{id}/{text_id}', [NotesController::class, 'update']);
     Route::delete('/text/{id}/{text_id}', [NotesController::class, 'destroy']);
